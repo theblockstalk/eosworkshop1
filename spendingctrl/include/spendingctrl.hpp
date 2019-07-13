8893a90623ff1b001818301449f36d5d6a840118
Jack <ugmo04@hotmail.com>
2019-07-13 12:53:49 +0100

    added balance

--- spendingctrl/include/spendingctrl.hpp
+++ spendingctrl/include/spendingctrl.hpp
@@ -2,8 +2,7 @@
 #include <eosio/print.hpp>
 #include <eosio/asset.hpp>
 #include <eosio/name.hpp>
-#include <eosio/system.hpp>
-// #include <eosio/singleton.hpp>
+#include <eosio/system.hpp> // needed for current_time_point()
 
 #include <string>
 
@@ -16,13 +15,18 @@ CONTRACT spendingctrl : public contract {
     
     spendingctrl(name receiver, name code, datastream<const char*> ds) : contract(receiver, code, ds) {}
     
+    // Set limits
     ACTION setdaylimit(name user, asset limit);
-    
+
+    // Withdrawals  
     ACTION withdrawal(const name user, const asset quantity, string& memo);
     
+    // Deposits
     [[eosio::on_notify("eosio.token::transfer")]] void ontransfer(const name from, const name to, const asset quantity, string& memo);
     
-    // A handy function while testing and developing a contract. Should not exist in final contract
+    // A handy function while testing and developing a contract
+    // Removes all table data, in case structure changes.
+    // Should not exist in final contract
     ACTION clear(name user);
 
   private:
@@ -30,6 +34,7 @@ CONTRACT spendingctrl : public contract {
     TABLE user {
       name user;
       asset limit;
+      asset balance;
       time_point_sec last_withdrawal;
       
       uint64_t primary_key() const { return user.value; }
--- spendingctrl/src/spendingctrl.cpp
+++ spendingctrl/src/spendingctrl.cpp
@@ -2,33 +2,43 @@
 
 void spendingctrl::withdrawal(const name user, const asset quantity, string& memo) {
   require_auth( user );
+  print("\n wallet withdrawal");
   
   auto self = get_self();
 
   auto sym = quantity.symbol.code();
   users_table users(self, sym.raw());
     
-    auto itr = users.find(user.value);
-    
-    if ( itr != users.end()) {
-      asset limit = itr->limit;
-      print("\n this token has a withdrawal limit of ", limit.to_string());
-      
+  auto itr = users.find(user.value);
+  
+  if ( itr != users.end()) {
+    asset limit = itr->limit;
+    // TODO: if (limit exists ) {
+          print("\n this user has a withdrawal limit of ", limit.to_string());
       eosio::check(limit.amount >= quantity.amount, "cannot withdraw more than limit");
-      eosio::check(itr->last_withdrawal < time_point_sec(current_time_point() - hours(24)), "cannot withdraw more than once per 24 hours");
-      
-      auto ram_payer = user;
-      users.modify(itr, ram_payer, [&](auto& row) {
-        row.last_withdrawal = current_time_point();
-      });
-      
-      action(
-        permission_level{get_self(), "active"_n}, // permission
-        "eosio.token"_n, // account "code"
-        "transfer"_n, // action
-        std::make_tuple(self, user, quantity, memo) // data
-      ).send();
-    }
+    // }
+
+    time_point_sec last_withdrawal = itr->last_withdrawal;
+    // TODO: if ( last_withdrawal exists ) {
+      eosio::check(last_withdrawal < time_point_sec(current_time_point() - hours(24)), "cannot withdraw more than once per 24 hours");
+    // }
+
+    asset balance = itr->balance;
+    eosio::check(balance.amount >= quantity.amount, "cannot withdraw more than balance");
+    
+    auto ram_payer = user;
+    users.modify(itr, ram_payer, [&](auto& row) {
+      row.last_withdrawal = current_time_point();
+    });
+  }
+  
+  // spendingctrl needs to have active permission of spendingctrl@eosio.code
+  action(
+    permission_level{get_self(), "active"_n}, // permission
+    "eosio.token"_n, // account to send action to "code"
+    "transfer"_n, // action
+    std::make_tuple(self, user, quantity, memo) // data
+  ).send();
 }
 
 void spendingctrl::ontransfer(const name from, const name to, const asset quantity, string& memo) {
@@ -38,8 +48,29 @@ void spendingctrl::ontransfer(const name from, const name to, const asset quanti
   
   if (to == self) {
     print("\n wallet deposit");
+    
+    auto sym = quantity.symbol.code();
+    users_table users(self, sym.raw());
+    
+    auto ram_payer = to;
+    
+    auto itr = users.find(to.value);
+    
+    if ( itr == users.end()) {
+      users.emplace(ram_payer, [&](auto& row) {
+        row.user = to;
+        row.balance = quantity;
+      });
+    } else {
+      users.modify(itr, ram_payer, [&](auto& row) {
+        row.balance += quantity;
+      });
+    }
   } else if (from == self) {
+    print("\n wallet withdrawal");
     // never triggered as require_recipient( from ) will not send notification when auth is from account from
+    
+    // user withdrawal() instead
   }
 }
 
@@ -49,7 +80,6 @@ void spendingctrl::setdaylimit(name user, asset limit) {
   require_auth(user);
   
   limit.is_valid();
-  // eosio::check(limit.symbol == symbol("EOS", 4), "must be EOS token");
 
   auto self = get_self();
   auto ram_payer = user;
