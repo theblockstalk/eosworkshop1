cb674ecfe9996703e6a386dfeef776fdc87ae3db
Jack Tanner <jack@jackandtheblockstalk>
2019-11-26 11:59:19 +0100

    created build folder

---
+++ helloworld11/contract/include/helloworld11.hpp
@@ -0,0 +1,24 @@
+#include <eosio/eosio.hpp>
+#include <eosio/system.hpp>
+#include <eosio/crypto.hpp>
+
+using namespace std;
+using namespace eosio;
+
+CONTRACT helloworld11 : public contract {
+  public:
+    using contract::contract;
+
+    ACTION hi(name from, const checksum256 &message_hash);
+    ACTION hiverify(name from, string message);
+    ACTION clear();
+
+  private:
+    TABLE messages {
+      name    user;
+      checksum256 hash;
+      time_point last_updated;
+      auto primary_key() const { return user.value; }
+    };
+    typedef multi_index<name("messages"), messages> messages_table;
+};
